850e89f44ad7dc4355eec1c5d03b47a3ef796f85
samuelchrist12 <christiawansamuel@gmail.com>
2021-11-05 08:56:31 +0700

    UI Update 1

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>@yield('title','Perpustakaan Lab UM')</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
 </head>
 <body class="warna">
     <nav class="navbar navbar-expand-lg navbar-light navatas">
